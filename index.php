c0731c20f751a111a39e068ffbda455772d4a55b
bksim <simpavid@gmail.com>
2013-01-26 17:36:06 -0500

    database....???

--- index.php
+++ index.php
@@ -184,38 +184,6 @@ $app_name = idx($app_info, 'name', '');
       </script>
     <![endif]-->
 
-    <?php
-    # This function reads your DATABASE_URL configuration automatically set by Heroku
-    # the return value is a string that will work with pg_connect
-    function pg_connection_string() {
-      return "dbname=daanlenp3al7n5 host=ec2-54-243-230-216.compute-1.amazonaws.com port=5432 user=cjykxetwjrzkrk password=jQ-kNfCjoVqqGbZi0NeM7GzurA sslmode=require";
-    }
-    # Establish db connection
-    $db = pg_connect(pg_connection_string());
-    if (!$db) {
-       echo "Database connection error."
-       exit;
-    }
-    $sqlcommand = "SELECT * FROM users";
-    $result = pg_query($db, $sqlcommand);
-    if (!$result) {
-      die("Error in SQL query: " . pg_last_error());
-    }
-
-    // iterate over result set
-    // print each row
-    while ($row = pg_fetch_array($result)) {
-       echo "FBID: " . $row[0] . "<br />";
-       echo "email: " . $row[1] . "<p />";
-    }
-
-    // free memory
-    pg_free_result($result);       
-
-    // close connection
-    pg_close($db);
-
-    ?>
 
   </head>
   <body>
@@ -260,6 +228,40 @@ $app_name = idx($app_info, 'name', '');
     </header>
       <?php if (isset($basic)) { ?>
 
+      <?php
+      # This function reads your DATABASE_URL configuration automatically set by Heroku
+      # the return value is a string that will work with pg_connect
+      function pg_connection_string() {
+        return "dbname=daanlenp3al7n5 host=ec2-54-243-230-216.compute-1.amazonaws.com port=5432 user=cjykxetwjrzkrk password=jQ-kNfCjoVqqGbZi0NeM7GzurA sslmode=require";
+      }
+      # Establish db connection
+      $db = pg_connect(pg_connection_string());
+      if (!$db) {
+         echo "Database connection error."
+         exit;
+      }
+      $sqlcommand = "SELECT * FROM users";
+      $result = pg_query($db, $sqlcommand);
+      if (!$result) {
+        die("Error in SQL query: " . pg_last_error());
+      }
+
+      // iterate over result set
+      // print each row
+      while ($row = pg_fetch_array($result)) {
+         echo "FBID: " . $row[0] . "<br />";
+         echo "email: " . $row[1] . "<p />";
+      }
+
+      // free memory
+      pg_free_result($result);       
+
+      // close connection
+      pg_close($db);
+
+      ?>
+
+    
       <div class="navbar navbar-inverse navbar-fixed-top">
         <div class="navbar-inner">
           <div class="container">

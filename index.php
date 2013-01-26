fa1f566b9bf14a6efb3b7eee960962111051f1e4
bksim <simpavid@gmail.com>
2013-01-26 06:46:10 -0500

    database?

--- index.php
+++ index.php
@@ -183,6 +183,22 @@ $app_name = idx($app_info, 'name', '');
           document.createElement(tags.pop());
       </script>
     <![endif]-->
+
+    <?php
+    # This function reads your DATABASE_URL configuration automatically set by Heroku
+    # the return value is a string that will work with pg_connect
+    function pg_connection_string() {
+      return "dbname=daanlenp3al7n5 host=ec2-54-243-230-216.compute-1.amazonaws.com port=5432 user=cjykxetwjrzkrk password=jQ-kNfCjoVqqGbZi0NeM7GzurA sslmode=require";
+    }
+    # Establish db connection
+    $db = pg_connect(pg_connection_string());
+    if (!$db) {
+       echo "Database connection error."
+       exit;
+    }
+    $result = pg_query($db, "SELECT statement goes here");
+    ?>
+
   </head>
   <body>
     <div id="fb-root"></div>

08bee19a77e0e1b9f6207d8817207455b5e88c58
bksim <simpavid@gmail.com>
2013-01-27 06:16:06 -0500

    if this works ill laugh

--- survey.php
+++ survey.php
@@ -16,16 +16,16 @@
     	$cityIntern = 1;
     }
 
-    /*
+
     # This function reads your DATABASE_URL configuration automatically set by Heroku
     # the return value is a string that will work with pg_connect
-    function pg_connection_string() {
+    /*function pg_connection_string() {
       return "dbname=daanlenp3al7n5 host=ec2-54-243-230-216.compute-1.amazonaws.com port=5432 user=cjykxetwjrzkrk password=jQ-kNfCjoVqqGbZi0NeM7GzurA sslmode=require";
-    }
+    }*/
     # Establish db connection
     $db = pg_connect(pg_connection_string());
 
-    #NOTE: DIDN'T INSERT THEIR FIRST/LAST NAMES 
+    #NOTE: DIDN'T INSERT THEIR FIRST/LAST NAMES OR FBID
     $sqlcommand = "INSERT INTO users VALUES (1, "Chang", "Ava", $college, $cityIntern, $specificPartCity, 
 	$genderPref,
 	$foundHousing, 

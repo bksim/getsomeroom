b1c71d08b667391d17c13e8691e45fc1a1164a8a
bksim <simpavid@gmail.com>
2013-01-27 08:31:42 -0500

    FINAL BUILD, SURVEY DONE DATABASE CONNECTED

--- submit.php
+++ submit.php
@@ -23,14 +23,6 @@
     $nightowl = 'f';
     $morningbird = 'f';
 
-    if ($cityIntern == "yes") {
-    	$cityIntern = 't';
-    }
-    else{
-    	$cityIntern = 'f';
-    }
-
-
     # This function reads your DATABASE_URL configuration automatically set by Heroku
     # the return value is a string that will work with pg_connect
     function pg_connection_string() {

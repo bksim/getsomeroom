19385f4bfe2275c177ab41b7a473d79a86c8eec5
bksim <simpavid@gmail.com>
2013-01-27 07:36:10 -0500

    ...

--- submit.php
+++ submit.php
@@ -30,12 +30,12 @@
 
     # This function reads your DATABASE_URL configuration automatically set by Heroku
     # the return value is a string that will work with pg_connect
-    function pg_connection_string() {
+    /*function pg_connection_string() {
       return "dbname=daanlenp3al7n5 host=ec2-54-243-230-216.compute-1.amazonaws.com port=5432 user=cjykxetwjrzkrk password=jQ-kNfCjoVqqGbZi0NeM7GzurA sslmode=require";
     }
     # Establish db connection
     $db = pg_connect(pg_connection_string());
-    
+    */
     /*
         #NOTE: DIDN'T INSERT THEIR FIRST/LAST NAMES OR FBID
     $query_insert = "INSERT INTO users VALUES (5,

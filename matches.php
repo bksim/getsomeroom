3bff952c811311112fa0372abf3fcea0942dba48
Shih Chang <achang4@wellesley.edu>
2013-01-28 14:30:48 -0500

    added scroll bar to matches

---
+++ matches.php
@@ -0,0 +1,33 @@
+
+
+
+<script> 
+  // GET STUFF FROM FACEBOOK, MAKE AJAX CALL TO DATABASE
+  FB.api('/me', function(response) {
+    $('#title').html("<h1>Time for you to getsomeroom, " + response.name + "</h1>");
+
+    $.post(
+      "database_connect.php",
+      { "name": response.name, "id": response.id},
+      function(data){
+        if (data){
+          //if data is not null
+          if (data === "<hr>")
+          {
+            data = "<hr>you have no matches :( #foreveralone";
+          }
+          $('#matches').html(data);
+        }
+      }
+    );
+  });
+</script>
+
+<div id="match-container" style="color:#FFFFFF;">
+  <div id="title"></div>
+  <div id="matches">
+    we're looking for your matches...
+  <div id="matches" style="overflow-y:auto;">
+    <img src="images/ajax_loader.gif" style="padding-left:50%;">
+  </div>
+</div>

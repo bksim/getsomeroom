338d04b65526e26093785de7fcdda64908f0ffb3
Shih Chang <achang4@wellesley.edu>
2013-01-28 14:45:27 -0500

    editing the about doood

--- about.php
+++ about.php
@@ -16,5 +16,5 @@
 
 </style>
   <div style="padding-left: 200px; padding-right: 200px; padding-top: 20px; font-weight: 900; overflow-y:auto; width: auto; height: 500px; color:#FFFFFF;">
-    <p class="lead">Did you get an awesome internship in a city? Excited? But maybe you need housing...and therefore a roomate, but have been unable to find anyone to room with! get a room solves that problem. Make a profile detailing where you plan to be, your preferences and what kind of housing you"re looking for. We"ll then match you up with potential roomates, starting out with Facebook friends and then branching out to people closest in degree with you! Try it out today! Feel free to ask us any questions you may have. We"re always here for you!</p>
+    <p class="lead"> Blah blah blah welcome to get some room i'll edit you later</p>
 </div> <!-- /container -->

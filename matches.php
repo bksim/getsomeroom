9e5cdc3d0df9381dc141b8ab4a25caacf0147a5c
Shih Chang <achang4@wellesley.edu>
2013-01-28 01:10:03 -0500

    enforce https

--- matches.php
+++ matches.php
@@ -7,6 +7,11 @@ require_once('AppInfo.php');
 // This provides access to helper functions defined in 'utils.php'
 require_once('utils.php');
 
+// Enforce https on production
+if (substr(AppInfo::getUrl(), 0, 8) != 'https://' && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
+  header('Location: https://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+  exit();
+}
 
 /*****************************************************************************
  *

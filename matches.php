031008e09018928e58cbe7d1c8cacb07bc66b483
Shih Chang <achang4@wellesley.edu>
2013-01-28 00:32:15 -0500

    fixing navigation with ajax

--- matches.php
+++ matches.php
@@ -1,5 +1,66 @@
-
 <?php
+
+// This provides access to helper functions defined in 'utils.php'
+require_once('utils.php');
+
+
+/*****************************************************************************
+ *
+ * The content below provides examples of how to fetch Facebook data using the
+ * Graph API and FQL.  It uses the helper functions defined in 'utils.php' to
+ * do so.  You should change this section so that it prepares all of the
+ * information that you want to display to the user.
+ *
+ ****************************************************************************/
+
+require_once('sdk/src/facebook.php');
+
+$facebook = new Facebook(array(
+  'appId'  => AppInfo::appID(),
+  'secret' => AppInfo::appSecret(),
+  'sharedSession' => true,
+  'trustForwarded' => true,
+));
+
+$user_id = $facebook->getUser();
+if ($user_id) {
+  try {
+    // Fetch the viewer's basic information
+    $basic = $facebook->api('/me');
+  } catch (FacebookApiException $e) {
+    // If the call fails we check if we still have a user. The user will be
+    // cleared if the error is because of an invalid accesstoken
+    if (!$facebook->getUser()) {
+      header('Location: '. AppInfo::getUrl($_SERVER['REQUEST_URI']));
+      exit();
+    }
+  }
+
+  // This fetches some things that you like . 'limit=*" only returns * values.
+  // To see the format of the data you are retrieving, use the "Graph API
+  // Explorer" which is at https://developers.facebook.com/tools/explorer/
+  $likes = idx($facebook->api('/me/likes?limit=4'), 'data', array());
+
+  // This fetches 4 of your friends.
+  $friends = idx($facebook->api('/me/friends?limit=4'), 'data', array());
+
+  // And this returns 16 of your photos.
+  $photos = idx($facebook->api('/me/photos?limit=16'), 'data', array());
+
+  // Here is an example of a FQL call that fetches all of your friends that are
+  // using this app
+  $app_using_friends = $facebook->api(array(
+    'method' => 'fql.query',
+    'query' => 'SELECT uid, name FROM user WHERE uid IN(SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1'
+  ));
+}
+
+// Fetch the basic info of the app that they are using
+$app_info = $facebook->api('/'. AppInfo::appID());
+
+$app_name = idx($app_info, 'name', '');
+?>
+
 <section id="samples" class="clearfix">
   <h1>Time for you to getsomeroom, <?php echo $basic['name'];?></h1>
 
@@ -91,4 +152,3 @@
     </ul>
   </div>
 </section>
-?>

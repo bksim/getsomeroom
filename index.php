5b22c4fe66fb7fed5fad2e7f2a8b24473e3217ed
bksim <simpavid@gmail.com>
2013-01-29 13:43:15 -0500

    testing maps

--- index.php
+++ index.php
@@ -87,21 +87,7 @@ if ($user_id) {
     <script type="text/javascript" src="javascript/jquery.cycle.all.js"></script> 
     <script src="javascript/bootstrap.min.js"></script>
 
-    <!-- MAPS FEATURE -->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDFtZJbKg9iT-Bg4zMFR-s0uJSuPwj5BVc&sensor=false"
-  type="text/javascript"></script>
-    <script type="text/javascript">
-      function initialize() {
-        console.log("IM HERE"); 
-        var mapOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 8,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map_canvas"),
-            mapOptions);
-      }
-    </script>
+
 
     <!-- testing cycle.js -->
     <script type="text/javascript">
--- map.php
+++ map.php
@@ -1,9 +1,29 @@
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+
     <style type="text/css">
       #map_canvas { height: 100% }
       #map_container {height: 100%;}
     </style>
 
-  <div id="map_container" onload="initialize()">
+    <!-- MAPS FEATURE -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDFtZJbKg9iT-Bg4zMFR-s0uJSuPwj5BVc&sensor=false" type="text/javascript"></script>
+
+    <script type="text/javascript">
+      function initialize() {
+        console.log("IM HERE"); 
+        var mapOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapOptions);
+      }
+
+      $(document).ready(function() {
+        initialize());
+      });
+    </script>
+
+  <div id="map_container">
     <div id="map_canvas" style="width:100%; height:100%"></div>
   </div>

b2c26302358e693c9c5ed8706e9977e19d91296f
bksim <simpavid@gmail.com>
2013-01-29 12:27:25 -0500

    fixed mail. final build

--- contact.php
+++ contact.php
@@ -1,5 +1,4 @@
-<?php
-
+<?php //NOTE: mail may not work in heroku but will work in all other cases
 // Contact subject
 $subject =$_POST['subject']; 
 
@@ -13,8 +12,6 @@ $name = $_POST['name'];
 // From 
 $header="from: '$name' <'$mail_from'>";
 
-echo $header;
-
 // Enter your email address
 $to ='bsim@college.harvard.edu';
 $send_contact=mail($to,$subject,$message,$header);
@@ -22,10 +19,12 @@ $send_contact=mail($to,$subject,$message,$header);
 // Check, if message sent to your email 
 // display message "We've recived your information"
 if($send_contact){
-echo "We've recived your contact information";
+echo "We've received your contact information";
+header('Location: index.php'); 
 }
 else {
-echo "ERROR";
+header('Location: index.php'); 
+//echo "ERROR";
 }
 
 ?>

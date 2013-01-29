a54cbb69ac00f9d228e2461dda66c81fac9065a5
bksim <simpavid@gmail.com>
2013-01-29 15:19:49 -0500

    testing

--- map.php
+++ map.php
@@ -75,9 +75,10 @@
 
       //returns latlongs for city names
       function geocodecity(city) {
-        var geocoder = new google.maps.Geocoder();
-        geocoder.geocode( { 'address': city}, function(results, status) {
+        var geocoder1 = new google.maps.Geocoder();
+        geocoder1.geocode( { 'address': city}, function(results, status) {
           if (status == google.maps.GeocoderStatus.OK) {
+            console.log(results[0].geometry.location)
             return results[0].geometry.location;
           } else {
             return "error";

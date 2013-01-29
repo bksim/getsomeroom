8101ac433584e16cc11f203e54a9e1982cc89594
bksim <simpavid@gmail.com>
2013-01-29 13:36:55 -0500

    testing map

--- map.php
+++ map.php
@@ -3,20 +3,7 @@
       #map_canvas { height: 100% }
       #map_container {height: 100%;}
     </style>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDFtZJbKg9iT-Bg4zMFR-s0uJSuPwj5BVc&sensor=false"
-  type="text/javascript"></script>
-    <script type="text/javascript">
-      function initialize() {
-        console.log("IM HERE");
-        var mapOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 8,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map_canvas"),
-            mapOptions);
-      }
-    </script>
+
   <div id="map_container" onload="initialize()">
     <div id="map_canvas" style="width:100%; height:100%"></div>
   </div>

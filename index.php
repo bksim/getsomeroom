ae101160e71bf5a382d0025f5701ca234d067cad
bksim <simpavid@gmail.com>
2013-01-29 10:58:42 -0500

    new jquery

--- index.php
+++ index.php
@@ -248,6 +248,59 @@ $.ajax({url:pageurl,success: function(data){
       }(document, 'script', 'facebook-jssdk'));
     </script>
 
+
+    <!-- Modal -->
+
+<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+    <h3 id="myModalLabel">Ask us a question!</h3>
+  </div>
+  <div class="modal-body">
+    
+    <table width="200" border="0" align="center" cellpadding="0" cellspacing="1">
+      <tr>
+      <td><form class ="navbar-form pull-left"name="form1" method="post" action="contact.php">
+      <table width="100%" border="0" cellspacing="1" cellpadding="3">
+        <tr>
+        <td width="16%">Subject      </td>
+        <td width="2%">:</td>
+        <td width="82%"><input name="subject" type="text" id="subject" size="50"></td>
+        </tr>
+        <tr>
+        <td>Detail      </td>
+        <td>:</td>
+        <td><textarea name="detail" cols="50" rows="4" id="detail"></textarea></td>
+        </tr>
+        <tr>
+        <td>Name     </td>
+        <td>:</td>
+        <td><input name="name" type="text" id="name" size="50"></td>
+        </tr>
+        <tr>
+        <td>Email     </td>
+        <td>:</td>
+        <td><input name="customer_mail" type="text" id="customer_mail" size="50"></td>
+        </tr>
+        <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td><input type="submit" name="Submit" value="Submit"> <input type="reset" name="Submit2" value="Reset"></td>
+        </tr>
+      </table>
+      </form>
+      </td>
+      </tr>
+    </table>
+
+  </div>
+  <div class="modal-footer">
+    <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
+    <button class="btn btn-primary">Submit</button>-->
+  </div>
+</div>
+
+
     <header class="clearfix">
     </header>
 

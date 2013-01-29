94f5d34d6b37351c355e15d9a25cc1a69611ac6d
bksim <simpavid@gmail.com>
2013-01-29 10:40:51 -0500

    testing modal

--- faq.php
+++ faq.php
@@ -1,105 +1,127 @@
-    <!-- Le styles -->
-    <style type="text/css">
-      body {
-        
-      }
+<!-- Le styles -->
+<style type="text/css">
+  body {
+    
+  }
 
-      /* Custom container */
-      .container-narrow {
-        margin: 0 auto;
-        max-width: 700px;
-      }
-      .container-narrow > hr {
-        margin: 10px 0;
-      }
+  /* Custom container */
+  .container-narrow {
+    margin: 0 auto;
+    max-width: 700px;
+  }
+  .container-narrow > hr {
+    margin: 10px 0;
+  }
 
-      .lead{
-        font-size: 17px;
-        font-style: bolder;
-        text-align: left;
-        color:#B23AEE;
-      }
+  .lead{
+    font-size: 17px;
+    font-style: bolder;
+    text-align: left;
+    color:#B23AEE;
+  }
 
-      .answer{
-      	font-size: 15px;
-        text-align: left;
-        font-style:normal;
-      }
+  .answer{
+  	font-size: 15px;
+    text-align: left;
+    font-style:normal;
+  }
 
-      /* Main marketing message and sign up button */
-      .jumbotron {
-        margin: 60px 0;
-        text-align: center;
-      }
-      .jumbotron h1 {
-        font-size: 72px;
-        line-height: 1;
-      }
-      .jumbotron .btn {
-        font-size: 21px;
-        padding: 14px 24px;
-      }
+  /* Main marketing message and sign up button */
+  .jumbotron {
+    margin: 60px 0;
+    text-align: center;
+  }
+  .jumbotron h1 {
+    font-size: 72px;
+    line-height: 1;
+  }
+  .jumbotron .btn {
+    font-size: 21px;
+    padding: 14px 24px;
+  }
 
-    </style>
+</style>
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
-        <div style="padding-left: 200px; padding-right: 200px; font-weight: 900; overflow-y:auto; padding-top: 20px; width: auto; height: 500px; color:#FFFFFF;">
+<div style="padding-left: 200px; padding-right: 200px; font-weight: 900; overflow-y:auto; padding-top: 20px; width: auto; height: 500px; color:#FFFFFF;">
 
-        <h2>FAQ</h2>
-        <p class="lead" >What is the purpose of getsomeroom?</p>
-        <p class="answer"> To meet new people who can possibly be your roommates for the summer! We find you matches based on the city you're working in as well as other things you tell us in the survey.</p>
-        
-        <p class="lead">What types of features are considered when matching roommates?</p>
-        <p class="answer">The city you will be interning in, gender and housing preferences, mutual friends, the specific parts of the city you would like to live in, your interests, and other things will all be taken into account. </p>
-        
-        <p class="lead">What do I do after I find my matches? </p>
-        <p class="answer"> We provide you with a limited profile of the other user, so once you determine who you might be interested in rooming with, click the message button and get ready to meet new people! </p>
-        
-        <p class="lead">How can I ensure this site is safe?</p>
-        <p class="answer"> Our site uses the HTTPS protocol, which take advantage of SSL, a cryptographic protocol that provides security. </p>
-        
-        <br>
+<h2>FAQ</h2>
+<p class="lead" >What is the purpose of getsomeroom?</p>
+<p class="answer"> To meet new people who can possibly be your roommates for the summer! We find you matches based on the city you're working in as well as other things you tell us in the survey.</p>
 
-        <table width="200" border="0" align="center" cellpadding="3" cellspacing="1">
-<tr>
-<td><strong>Contact Form </strong></td>
-</tr>
-</table>
-<table width="200" border="0" align="center" cellpadding="0" cellspacing="1">
-<tr>
-<td><form class ="navbar-form pull-left"name="form1" method="post" action="contact.php">
-<table width="100%" border="0" cellspacing="1" cellpadding="3">
-<tr>
-<td width="16%">Subject      </td>
-<td width="2%">:</td>
-<td width="82%"><input name="subject" type="text" id="subject" size="50"></td>
-</tr>
-<tr>
-<td>Detail      </td>
-<td>:</td>
-<td><textarea name="detail" cols="50" rows="4" id="detail"></textarea></td>
-</tr>
-<tr>
-<td>Name     </td>
-<td>:</td>
-<td><input name="name" type="text" id="name" size="50"></td>
-</tr>
-<tr>
-<td>Email     </td>
-<td>:</td>
-<td><input name="customer_mail" type="text" id="customer_mail" size="50"></td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-<td><input type="submit" name="Submit" value="Submit"> <input type="reset" name="Submit2" value="Reset"></td>
-</tr>
-</table>
-</form>
-</td>
-</tr>
-</table>
+<p class="lead">What types of features are considered when matching roommates?</p>
+<p class="answer">The city you will be interning in, gender and housing preferences, mutual friends, the specific parts of the city you would like to live in, your interests, and other things will all be taken into account. </p>
+
+<p class="lead">What do I do after I find my matches? </p>
+<p class="answer"> We provide you with a limited profile of the other user, so once you determine who you might be interested in rooming with, click the message button and get ready to meet new people! </p>
+
+<p class="lead">How can I ensure this site is safe?</p>
+<p class="answer"> Our site uses the HTTPS protocol, which take advantage of SSL, a cryptographic protocol that provides security. </p>
+
+<br>
+
+<!-- Button to trigger modal -->
+<a href="#myModal" role="button" class="btn" data-toggle="modal">Have a question?</a>
+ 
+<!-- Modal -->
+<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+    <h3 id="myModalLabel">Ask us a question!</h3>
+  </div>
+  <div class="modal-body">
+    
+    <table width="200" border="0" align="center" cellpadding="0" cellspacing="1">
+      <tr>
+      <td><form class ="navbar-form pull-left"name="form1" method="post" action="contact.php">
+      <table width="100%" border="0" cellspacing="1" cellpadding="3">
+        <tr>
+        <td width="16%">Subject      </td>
+        <td width="2%">:</td>
+        <td width="82%"><input name="subject" type="text" id="subject" size="50"></td>
+        </tr>
+        <tr>
+        <td>Detail      </td>
+        <td>:</td>
+        <td><textarea name="detail" cols="50" rows="4" id="detail"></textarea></td>
+        </tr>
+        <tr>
+        <td>Name     </td>
+        <td>:</td>
+        <td><input name="name" type="text" id="name" size="50"></td>
+        </tr>
+        <tr>
+        <td>Email     </td>
+        <td>:</td>
+        <td><input name="customer_mail" type="text" id="customer_mail" size="50"></td>
+        </tr>
+        <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td><input type="submit" name="Submit" value="Submit"> <input type="reset" name="Submit2" value="Reset"></td>
+        </tr>
+      </table>
+      </form>
+      </td>
+      </tr>
+    </table>
+
+  </div>
+  <div class="modal-footer">
+    <button class="btn" data-dismiss="modal" aria-hidden="true">Submit</button>
+    <!--<button class="btn btn-primary">Save changes</button>-->
+  </div>
+</div>
+
+<!--
+
+<table width="200" border="0" align="center" cellpadding="3" cellspacing="1">
+  <tr>
+    <td><strong>Contact Form </strong></td>
+  </tr>
+</table>-->
+
 

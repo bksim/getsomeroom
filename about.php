243a441821e9034b0086b70b414ace311c7060eb
Shih Chang <achang4@wellesley.edu>
2013-01-29 04:02:25 -0500

    added some stuff

--- about.php
+++ about.php
@@ -19,7 +19,7 @@
     <p class="lead"> This site was created for the 6.470 (Web Programming Design Class) at MIT during the IAP 2013 period. After spending 
       countless hours finding an internship/job for the winter or summer, we often spend even more countless hours 
       figuring out where we will live and who we will room with. Finding housing is difficult, but finding roommates is even more 
-      difficult. This site facilitates roommmate-finding by asking each user where they will intern, which gender they prefer 
+      difficult. This site facilitates roommate-finding by asking each user where they will intern, which gender they prefer 
       to room with, and what college they attend, among other questions. Users are then matched with other users on the site depending
       on the data that they entered. Our hope is that this site will ease your roommate-searching 
       quest by connecting you with potential choices. Even if you dont end up rooming with the people you meet on getsomeroom, 
